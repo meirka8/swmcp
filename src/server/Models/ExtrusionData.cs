83529ac52a9360a73a3526b35a60b630dca8faa2
meirka8 <67155085+meirka8@users.noreply.github.com>
2025-12-23 23:21:43 +0200

    feat: Add initial configuration for known features and their properties.

--- src/server/Models/ExtrusionData.cs
+++
@@ -1,8 +0,0 @@
-namespace swmcp.server.Models
-{
-    public class ExtrusionData : FeatureData
-    {
-        public double Depth { get; set; }
-        public double ReverseDepth { get; set; }
-    }
-}
